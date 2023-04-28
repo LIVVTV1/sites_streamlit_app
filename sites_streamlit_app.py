53df01e19b1441014cd37a5cd3658b6de9a595e2
LIVVTV1 <132046654+LIVVTV1@users.noreply.github.com>
2023-04-28 10:49:48 +0300

    Create sites_streamlit_app.py
    Travel objectives added

---
+++ sites_streamlit_app.py
@@ -0,0 +1,11 @@
+import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
+streamlit.title('Best sites to visit')
+streamlit.header('Travel objectives')
+streamlit.text('🪂 Things to do')
+streamlit.text('🏞️🥗Sites to visit')
+streamlit.text('🍲Traditional food')
