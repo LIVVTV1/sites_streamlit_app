a12933ce876db83be6bc197107ee46485aa5a9c1
LIVVTV1 <132046654+LIVVTV1@users.noreply.github.com>
2023-04-28 13:56:05 +0300

    Update sites_streamlit_app.py
    csv

--- sites_streamlit_app.py
+++ sites_streamlit_app.py
@@ -13,7 +13,9 @@ streamlit.text('🍲 Traditional food')
 streamlit.header('🏖️ Choose your holiday destination ⛵')
 
 #import pandas
-my_country_list=pandas.read_csv("https://github.com/ajturner/acetate/blob/master/places/Countries-Europe.csv", sep=',')
+my_country_list=pandas.read_csv("https://github.com/LIVVTV1/sites_streamlit_app/blob/main/Countries-Europe.csv", sep=',')
+#https://github.com/ajturner/acetate/blob/master/places/Countries-Europe.csv
+
 my_country_list=my_fruit_list.set_index('name')
 
 countries_selected = streamlit.multiselect("Pick your destination:", list(my_country_list.index),['France','Italy'])
