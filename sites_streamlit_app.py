df0ccfd9f624889ddd0ac681235958f99f35d00d
LIVVTV1 <132046654+LIVVTV1@users.noreply.github.com>
2023-05-03 15:44:48 +0300

    Update sites_streamlit_app.py

--- sites_streamlit_app.py
+++ sites_streamlit_app.py
@@ -55,7 +55,7 @@ try:
    
     country_dict=json.loads(countries_json)
     # output in the screen as a table
-    streamlit.dataframe(country_dict)
+    streamlit.print(country_dict)
     
     #back_from_function=get_country_data(country_choice)
     #streamlit.dataframe(back_from_function)
