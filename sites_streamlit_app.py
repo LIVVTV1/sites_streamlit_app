0b4f3fa558d68e57f6c6b7f2a2904bdecbab62cc
LIVVTV1 <132046654+LIVVTV1@users.noreply.github.com>
2023-05-03 15:12:21 +0300

    Update sites_streamlit_app.py

--- sites_streamlit_app.py
+++ sites_streamlit_app.py
@@ -50,7 +50,7 @@ try:
     
     #take the json version of the response and normalize it
     #country_normalized = pandas.json_normalize(country_response.json())
-    country_normalized = pandas.json_normalize(countries_json.json())
+    #country_normalized = pandas.json_normalize(countries_json.json())
    
     # output in the screen as a table
     streamlit.dataframe(country_normalized)
