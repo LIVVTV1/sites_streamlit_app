87ace052a2c17cd256452bd04c38f40a5ddfe7b8
LIVVTV1 <132046654+LIVVTV1@users.noreply.github.com>
2023-05-04 13:30:40 +0300

    Update sites_streamlit_app.py

--- sites_streamlit_app.py
+++ sites_streamlit_app.py
@@ -52,7 +52,7 @@ try:
     
     #take the json version of the response and normalize it
    
-    country_normalized = pandas.json_normalize(countries_json)
+    country_normalized = pandas.json_normalize(countries_json['sites'])
     
    
  
