91fa4ab552a025e2164dad824a5d493406366d6a
LIVVTV1 <132046654+LIVVTV1@users.noreply.github.com>
2023-05-05 10:18:04 +0300

    Update sites_streamlit_app.py

--- sites_streamlit_app.py
+++ sites_streamlit_app.py
@@ -105,10 +105,33 @@ try:
  }
 ]
 
-    df=pandas.json_normalize(dic)
-    
+    #df=pandas.json_normalize(dic)
+    #countries_to_show2 =df.loc[country_choice]    
     # output in the screen as a table
-    streamlit.dataframe(df)
+    #streamlit.dataframe(countries_to_show2)
+    
+    
+    def flatten_json(y):
+    out = {}
+
+    def flatten(x, name=''):
+        if type(x) is dict:
+            for a in x:
+                flatten(x[a], name + a + '_')
+        elif type(x) is list:
+            i = 0
+            for a in x:
+                flatten(a, name + str(i) + '_')
+                i += 1
+        else:
+            out[name[:-1]] = x
+
+    flatten(y)
+    return out
+  
+   flat = flatten_json(dic)
+   json_normalize(flat)
+  
 
     
     #back_from_function=get_country_data(country_choice)
